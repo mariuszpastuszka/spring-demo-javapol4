084b95db62130746f423448639702cb32f433936
maniek <mpastuszka@gmail.com>
2020-12-05 12:10:38 +0100

    [add] - rest controller - read one coffee machine

--- src/main/java/pl/sda/javapol4/springdemojavapol4/controller/RestCoffeeMachineController.java
+++ src/main/java/pl/sda/javapol4/springdemojavapol4/controller/RestCoffeeMachineController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.javapol4.springdemojavapol4.entity.CoffeeMachine;
@@ -24,7 +25,7 @@ public class RestCoffeeMachineController {
 
     // Jackson
 //    @GetMapping("/all-coffee-machines")
-    @GetMapping(value = "/all-coffee-machines",
+    @GetMapping(value = "/coffee-machines",
         produces = {MediaType.APPLICATION_JSON_VALUE,
         MediaType.APPLICATION_XML_VALUE})
     public List<CoffeeMachine> myCoffeeMachines() {
@@ -32,11 +33,38 @@ public class RestCoffeeMachineController {
         return coffeeMachineService.giveMeMyCoffeeMachines();
     }
 
+    // /coffee-machines/1
+    // /coffee-machines/250
+    @GetMapping("/coffee-machines/{id}")
+    public CoffeeMachine findCoffeeMachineById(@PathVariable("id") long idik) {
+        log.info("find coffee machine by id: [{}]", idik);
+        // ...
+        return null;
+    }
+
+    // /coffee-machines/{producer}/model/{name}
+    @GetMapping("/coffee-machines/{producer}/model/{name}")
+    public CoffeeMachine findByProducerAndModelName(
+        @PathVariable("producer") String producer, @PathVariable("name") String modelName) {
+        log.info("find coffee machine by producer: [{}] and model name [{}]",
+            producer, modelName);
+        return null;
+    }
     /** - my json
      * [
      *  {
      *      "key": value
      *  }
      * ]
+     */
+
+    /**
+     * CRUD
+     * Read - GET /coffee-machines - all
+     * Read One - GET /coffee-machines/{id} - one
+     * Create - POST - /coffee-machines
+     * Delete - DELETE /coffee-machines/{id}
+     * Update - PATCH - partial update /coffee-machines/{id}
+     * Update - PUT - replace /coffee-machines/{id}
      */
 }
